7ba5532324831fb5611e0449eb5392971a39e481
Shayaan Sultan <sultan.shayaan@gmail.com>
2025-04-26 16:08:47 -0700

    deployed contracts

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,8 +1,17 @@
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 
+dotenv.config();
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.29",
+  networks: {
+    amoy: {
+      url: process.env.AMOY_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
 
 export default config;
