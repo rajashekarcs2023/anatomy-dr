0a136dec7e9ac1b3508933d83eeb8a3df5dfdc49
Shayaan Sultan <sultan.shayaan@gmail.com>
2025-04-26 16:28:58 -0700

    add etherscan

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -4,12 +4,21 @@ import { HardhatUserConfig } from "hardhat/config";
 
 dotenv.config();
 
+console.log("AMOY_URL", process.env.AMOY_URL);
+console.log("PRIVATE_KEY", process.env.PRIVATE_KEY);
+console.log("POLYGONSCAN_API_KEY", process.env.POLYGONSCAN_API_KEY);
+
 const config: HardhatUserConfig = {
   solidity: "0.8.29",
   networks: {
     amoy: {
       url: process.env.AMOY_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonAmoy: process.env.POLYGONSCAN_API_KEY || "",
     },
   },
 };
